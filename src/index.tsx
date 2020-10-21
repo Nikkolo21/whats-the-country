2ae5424b7fb37752cc98544426f069ab2619bd7f
luis <nikko1801@gmail.com>
2020-10-21 01:56:27 -0500

    adding elements per page

--- src/index.tsx
+++ src/index.tsx
@@ -20,11 +20,11 @@ const client = new ApolloClient({
 })
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ApolloProvider>,
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
--- src/views/ListCountries/index.tsx
+++ src/views/ListCountries/index.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/react-hooks';
-import gql from "graphql-tag";
+import { gql } from '@apollo/client';
 
-const GET_COUNTRY_INFO = gql`{
-    Country {
-        name,
-        alpha2Code,
-        officialLanguages {
-            name
-        },
-        currencies {
-            name
-        },
-        subregion {
+const GET_COUNTRY_INFO = gql`
+    query($first: Int, $offset: Int) {
+        Country(first: $first, offset: $offset) {
             name,
-            region {
+            alpha2Code,
+            officialLanguages {
                 name
+            },
+            currencies {
+                name
+            },
+            subregion {
+                name,
+                region {
+                    name
+                }
             }
         }
-    }
-}`
+    }`;
 
 interface Country {
     alpha2Code: string,
@@ -39,7 +39,10 @@ type ListCountriesProps = {
 }
 
 function ListCountries({id}: ListCountriesProps) {
-    const { data, loading, error } = useQuery<CountriesData>(GET_COUNTRY_INFO);
+    const [pageSize, setPageSize] = useState<number>(20);
+    const [pageOffset, setPageOffset] = useState<number>(1);
+    const { data, loading, error } = useQuery<CountriesData>(GET_COUNTRY_INFO, {variables: {first: pageSize, offset: pageOffset}});
+
     if (data) console.log(data);
     return (
         <section>
@@ -48,15 +51,21 @@ function ListCountries({id}: ListCountriesProps) {
                     <p>...loading</p>
                 ) : (
                     <>
-                    {
-                        data && data.Country.map(elem => {
-                            return <div>
-                                <p>
-                                    {elem.name}
-                                </p>
-                            </div>
-                        })
-                    }
+                        {
+                            data && data.Country && data.Country.map(elem => {
+                                return <div key={elem.alpha2Code}>
+                                    <p>
+                                        {elem.name}
+                                    </p>
+                                </div>
+                            })
+                        }
+                        <select value={pageSize} onChange={e => setPageSize(Number(e.target.value))}>
+                            <option value={10}>10</option>
+                            <option value={20}>20</option>
+                            <option value={30}>30</option>
+                            <option value={40}>40</option>
+                        </select>
                     </>
                 )
             }
