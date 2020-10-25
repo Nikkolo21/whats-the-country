dcfc9c5171195225747b3cdba129c819943cd290
luis <nikko1801@gmail.com>
2020-10-25 16:50:31 -0500

    adding test to Dropdown and Button components

--- src/components/shared/Button/Button.test.tsx
+++ src/components/shared/Button/Button.test.tsx
@@ -10,4 +10,10 @@ describe("Rendering Button component", () => {
   it("render Button Component without crashing", () => {
     shallow(<Button {...ButtonProps}/>);
   })
+
+  it("expect that Button component receive the right title", () => {
+    const button = shallow(<Button {...ButtonProps}/>);
+    const buttonTitle = button.text();
+    expect(buttonTitle).toEqual(ButtonProps.title);
+  })
 });
